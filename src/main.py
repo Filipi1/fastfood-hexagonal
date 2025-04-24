e4761ccf7493c9039408a6ec77ccd25cf953b3cc
Filipe <filipebragabrasil@hotmail.com>
2025-04-23 23:32:20 -0300

    fix: fixed application bootstrap and removed unused data

--- src/main.py
+++ src/main.py
@@ -1,7 +1,4 @@
-import logging
-
 from dotenv import load_dotenv
-from fastapi.security import OAuth2PasswordBearer
 from fastapi import FastAPI
 
 from modules.infra.dependency_injector.containers import CoreContainer
@@ -9,21 +6,14 @@ from modules.shared.adapters.presentation.decorators import FastAPIManager
 
 load_dotenv(override=True)
 
-
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-
 container = CoreContainer()
 container.wire(modules=[__name__])
-
 app = FastAPI(
     title="API",
-    description="API para o sistema de e-commerce",
-    version="1.0.0",
+    description="API para o sistema de fastfood",
+    version="0.1.0",
     docs_url="/docs",
     prefix="/api",
 )
 
 FastAPIManager.initialize(app)
-
-logger = logging.getLogger("fastapi")
-logger.log(level=logging.INFO, msg="Teste!")
