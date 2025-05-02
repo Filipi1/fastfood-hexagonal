6590cf6c09fa976e00c95fc068d51753024a7418
Filipe <filipebragabrasil@hotmail.com>
2025-05-02 17:47:21 -0300

    feat: updated format in auth controller

--- src/modules/authentication/adapters/presentation/controllers/auth_controller.py
+++ src/modules/authentication/adapters/presentation/controllers/auth_controller.py
@@ -1,4 +1,4 @@
-from http import HTTPMethod
+from http import HTTPMethod, HTTPStatus
 
 from fastapi import HTTPException
 from modules.authentication.adapters.dtos import RequestAnonymousLogin, RequestLogin
@@ -19,16 +19,30 @@ class AuthController(APIController):
         self.__authenticate_use_case = AuthenticationContainer.authenticate_use_case()
         super().__init__()
 
-    @FastAPIManager.route("/login/anonymous", method=HTTPMethod.POST)
+    @FastAPIManager.route(
+        "/login/anonymous",
+        method=HTTPMethod.POST,
+        summary="Efetua login anonimo",
+        description="Efetua login anonimo",
+    )
     async def login_anonymous(self, anonymous_data: RequestAnonymousLogin):
         token = await self.__authenticate_use_case.process(anonymous_data)
         if token is None:
-            raise HTTPException(status_code=401, detail={"message": "Acesso negado"})
+            raise HTTPException(
+                status_code=HTTPStatus.UNAUTHORIZED, detail={"message": "Acesso negado"}
+            )
         return {"message": "Autenticado com sucesso", "data": {"token": token}}
 
-    @FastAPIManager.route("/login", method=HTTPMethod.POST)
+    @FastAPIManager.route(
+        "/login",
+        method=HTTPMethod.POST,
+        summary="Efetua login com usuario e senha",
+        description="Efetua login com usuario e senha",
+    )
     async def login(self, login_data: RequestLogin):
         token = await self.__authenticate_use_case.process(login_data)
         if token is None:
-            raise HTTPException(status_code=401, detail={"message": "Acesso negado"})
+            raise HTTPException(
+                status_code=HTTPStatus.UNAUTHORIZED, detail={"message": "Acesso negado"}
+            )
         return {"message": "Autenticado com sucesso", "data": {"token": token}}
